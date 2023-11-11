4b05e302d5e3c00bcd10816e69ecee2904aa1645
RAZU-sheikh <razu351724@gmail.com>
2023-11-12 03:50:32 +0600

    jwt token secret and set token to Http only cookie

---
+++ _jwt_steps_.js
@@ -0,0 +1,21 @@
+/**
+ * install jsonwebtoken
+ * jwt.sign (payload, secret, {expiresIt:})
+ * token client
+ */
+
+
+/**
+ * how to store token in the client side
+ * 1. memory --> ok type
+ * 2. local storage --> Ok type (xss)
+ * 3. cookies: http only
+ */
+
+/**
+ * 1. set cookies with http only. for development secure: false,
+ * 
+ * 2. cors
+ * 
+ * 3. client side axios setting
+ */
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -12,7 +12,9 @@ app.use(express.json());
 console.log(process.env.DB_PASS)
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1yeyxk.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1yeyxk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1yeyxk.mongodb.net/yourDatabaseName?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +32,20 @@ async function run() {
 
     const serviceCollection = client.db('safeFood').collection('myitems');
     const itemsCollection = client.db('safeFood').collection('bookitem');
+
+    // auth related api 
+    app.post("/jwt", async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h"})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false, // http://localhost:5174/login
+        sameSite: "none"
+      })
+      .send({Succcess:true})
+    })
 
     app.get('/myitems', async (req, res) => {
         const cursor = serviceCollection.find();
