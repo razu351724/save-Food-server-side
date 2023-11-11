d5f2a8edd5331e2c34671984eea8819097698145
RAZU-sheikh <razu351724@gmail.com>
2023-11-12 01:02:47 +0600

    delete api and delet aserver site

--- index.js
+++ index.js
@@ -27,12 +27,75 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('safeFood').collection('myitems');
+    const itemsCollection = client.db('safeFood').collection('bookitem');
+
+    app.get('/myitems', async (req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
+      app.get('/myitems/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+  
+        const options = {
+          // Include only the `title` and `imdb` fields in the returned document
+          projection: { title: 1, price: 1, id: 1, img: 1, name:1 },
+        };
+  
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+      })
+
+      app.get('/bookitem', async(req, res) => {
+        console.log(req.query.email);
+        let query = {};
+        if(req.query?.email){
+          query = {email: req.query.email}
+        }
+  
+        // দুই লাইনের কোড এক লাইন করছি
+        const result = await itemsCollection.find(query).toArray();
+        res.send(result);
+      })
+
+      app.post('/bookitem', async(req, res) => {
+          const body= req.body
+          console.log(body)
+          const result = await itemsCollection.insertOne(body)
+          res.send(result)
+      })
+
+      app.patch('/bookitem/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updatedBooking = req.body;
+        console.log(updatedBooking);
+        const updateDoc = {
+          $set: {
+            status: updatedBooking.status
+          },
+        };
+        const result = await itemsCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      })
+
+      app.delete('/bookitem/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await itemsCollection.deleteOne(query);
+        res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
